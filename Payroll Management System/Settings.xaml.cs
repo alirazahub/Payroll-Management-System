5e85f2a159f0e0b5c9980bab77a149cfc6081c66
alirazahub <aliraza.ar7785@gmail.com>
2023-01-01 23:46:16 +0500

    2 screens left

--- Payroll Management System/Settings.xaml.cs
+++ Payroll Management System/Settings.xaml.cs
@@ -48,15 +48,11 @@ namespace Payroll_Management_System
 
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {
+            Frame.Navigate(typeof(CustomSearching));
 
         }
 
         private void Button_Click_6(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_7(object sender, RoutedEventArgs e)
         {
 
         }
